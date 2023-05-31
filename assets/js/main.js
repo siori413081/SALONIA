f331f0f1848d63f60c459cec06b581b62a74f802
ShioriItai <siycha@gmail.com>
2023-06-01 00:54:21 +0900

    2023/06/01

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,29 @@
-
+/*ハンバーガメニュー */
 $(".sp-open-btn").click(function () {
     $(this).toggleClass('active');
+    $('.sp-menu').toggleClass('active');
+    $('.header-sp').toggleClass('active');
+});
+
+/*hero slider*/
+$('.slider').slick({
+    fade:true,//切り替えをフェードで行う。初期値はfalse。
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    autoplaySpeed: 3000,//次のスライドに切り替わる待ち時間
+    speed:1000,//スライドの動きのスピード。初期値は300。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 1,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    dots: true,//下部ドットナビゲーションの表示
+    pauseOnFocus: false,//フォーカスで一時停止を無効
+    pauseOnHover: false,//マウスホバーで一時停止を無効
+    pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+    arrows: false,
+    dotsClass: "slide-dots"
+
+});
+
+//スマホ用：スライダーをタッチしても止めずにスライドをさせたい場合
+$('.slider').on('touchmove', function(event, slick, currentSlide, nextSlide){
+$('.slider').slick('slickPlay');
 });
