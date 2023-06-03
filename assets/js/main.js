5dfb4602f822d6ff964188cb6ae60aec0a85a083
ShioriItai <siycha@gmail.com>
2023-06-04 00:30:58 +0900

    2023/06/03

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,6 +41,19 @@ $(function(){
     });
 });
 
+$(function(){
+    $('.support-hover-btn').hover(function(){
+        $('.support-hover').toggleClass('active');
+    });
+});
+
+$(function(){
+    $('.onlineShop-hover-btn').hover(function(){
+        $('.onlineShop-hover').toggleClass('active');
+    });
+});
+
+
 /* header スクロールしたら途中から固定 */
 function FixedAnime() {
 	var elemTop = $('.topics').offset().top;//.topicsの位置まできたら
@@ -80,20 +93,6 @@ function slideAnime(){
 }
 
 
-// eachTextAnimeにappeartextというクラス名を付ける定義
-function EachTextAnimeControl() {
-$('.eachTextAnime').each(function () {
-	var elemPos = $(this).offset().top - 50;
-	var scroll = $(window).scrollTop();
-	var windowHeight = $(window).height();
-	if (scroll >= elemPos - windowHeight) {
-	$(this).addClass("appeartext");
-
-	} else {
-	$(this).removeClass("appeartext");
-	}
-});
-}
 
 // 画面をスクロールをしたら動かしたい場合の記述
 $(window).scroll(function () {
@@ -122,3 +121,112 @@ $(function(){
 		$('.arrow.arrow-right').toggleClass('active');
 	});
 });
+
+/**下からひゅっ */
+function fadeAnime() {
+	var time = 0.5;//遅延時間を増やす秒数の値
+	var value = time;
+	$('.delayScroll').each(function () {
+		var parent = this;					//親要素を取得
+		var elemPos = $(this).offset().top;//要素の位置まで来たら
+		var scroll = $(window).scrollTop();//スクロール値を取得
+		var windowHeight = $(window).height();//画面の高さを取得
+		var childs = $(this).children();	//子要素を取得
+
+		if (scroll >= elemPos - windowHeight && !$(parent).hasClass("play")) {//指定領域内にスクロールが入ったらまた親要素にクラスplayがなければ
+			$(childs).each(function () {
+
+				if (!$(this).hasClass("fadeUp")) {//アニメーションのクラス名が指定されているかどうかをチェック
+
+					$(parent).addClass("play");	//親要素にクラス名playを追加
+					$(this).css("animation-delay", value + "s");//アニメーション遅延のCSS animation-delayを追加し
+					$(this).addClass("fadeUp");//アニメーションのクラス名を追加
+					value = value + time;//delay時間を増加させる
+
+					//全ての処理を終わったらplayを外す
+					var index = $(childs).index(this);
+					if((childs.length-1) == index){
+						$(parent).removeClass("play");
+					}else {
+						$(childs).removeClass("fadeUp");//アニメーションのクラス名を削除
+						value = time;//delay初期値の数値に戻す
+					}
+				}
+			})
+		}
+	})
+//じわっとする
+	$('.blurTrigger').each(function(){ //blurTriggerというクラス名が
+		var elemPos = $(this).offset().top-50; //要素より、50px上の
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight){
+		$(this).addClass('blur');
+		// 画面内に入ったらfadeDownというクラス名を追記
+		}else{
+		$(this).removeClass('blur');
+		// 画面外に出たらfadeDownというクラス名を外す
+		}
+		});
+
+//左からふわっ
+		$('.fadeLeftTrigger').each(function(){ //fadeLeftTriggerというクラス名が
+			var elemPos = $(this).offset().top-50; //要素より、50px上の
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll >= elemPos - windowHeight){
+			$(this).addClass('fadeLeft');
+			// 画面内に入ったらfadeDownというクラス名を追記
+			}else{
+			$(this).removeClass('fadeLeft');
+			// 画面外に出たらfadeDownというクラス名を外す
+			}
+			});
+
+
+}
+
+
+
+//文字が流れる
+function EachTextAnimeControl() {
+	$('.eachTextAnime').each(function () {
+		var elemPos = $(this).offset().top - 50;
+		var scroll = $(window).scrollTop();
+		var windowHeight = $(window).height();
+		if (scroll >= elemPos - windowHeight) {
+		$(this).addClass("appeartext");
+		} else {
+		$(this).removeClass("appeartext");
+		}
+		$(window).scroll(function () {
+			EachTextAnimeControl();/* アニメーション用の関数を呼ぶ*/
+		});
+		 //spanタグを追加する
+		var element = $(".eachTextAnime");
+		element.each(function () {
+			var text = $(this).text();
+			var textbox = "";
+			text.split('').forEach(function (t, i) {
+			if (t !== " ") {
+				if (i < 10) {
+				textbox += '<span style="animation-delay:.' + i + 's;">' + t + '</span>';
+				} else {
+				var n = i / 10;
+				textbox += '<span style="animation-delay:' + n + 's;">' + t + '</span>';
+				}
+
+			}
+			});
+			$(this).html(textbox);
+		});
+	});
+}
+
+
+
+// 画面をスクロールをしたら動かしたい場合の記述
+	$(window).scroll(function (){
+		fadeAnime();/* アニメーション用の関数を呼ぶ*/
+	});// ここまで画面をスクロールをしたら動かしたい場合の記述
+
